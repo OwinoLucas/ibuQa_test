ef86798e4265bc02d3ec4411488fb9208eea5514
OwinoLucas <lucowish35@gmail.com>
2022-05-14 20:53:13 +0300

    started project

---
+++ test_service/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages import constants as messages
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse, Http404, HttpResponseNotAllowed
+from .forms import *
+from .models import *
+
+# Create your views here.
+def home(request):
+   
+    context = { }
+    return render(request, 'index.html', context)
